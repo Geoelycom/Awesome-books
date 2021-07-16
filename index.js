78174a47b07db5d14191af851b821df58457e1ad
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-07-16 17:11:01 +0100

    Add js file

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 //console.log('hello world');
-function Books(author, bookName, pages, read) {
+function Books(author, bookName, pages, status) {
 	this.author = author;
 	this.bookName = bookName;
 	this.pages = pages;
-	this.read = read;
+	this.status = status;
 	this.bookInfo = function () {
-		return `the ${bookName} is written by ${author} it contains ${pages} and i ${read} `;
+		return `the ${bookName} is written by ${author} it contains ${pages} and i ${status} `;
 	}
 	// bookInfo();
 }
 //Books.bookInfo();
 
+const tableBody = document.querySelector('.tbody');
+const form = document.querySelector('.form-body');
+console.log(form);
+
+
+const libraryData = [
+	{ author: 'james alen', bookName: 'As a man thinketh', pages: 789, status: 'read' },
+	{ author: 'markle bloom', bookName: 'Love on the road', pages: 500, status: 'not read' },
+	{ author: 'charles duhig', bookName: 'the power of habit', pages: 400, status: 'not read' },
+	{ author: 'sarah bloomberg', bookName: 'a tail of two cities', pages: 340, status: 'read' }
+
+]
+
+function displayBooks() {
+	libraryData.forEach(books => {
+		tableBody.innerHTML += `<tr>
+		<td>${books.bookName}</td>
+		<td>${books.author}</td>
+		<td>${books.status}</td>
+	</tr> `
+		console.log(books)
+	})
+}
+console.log(displayBooks());
 
 
 const book1 = new Books('john doe', 'I love to play football');
 const book2 = new Books('richard brandson', 'the way we understand how to make money', '250 pages', 'have not read this book yet');
 book1.bookInfo();
 console.log(book2.bookInfo());
+
+
+// const input = document.querySelector('.title');
+
+// const contentWrapper = document.createElement('p')
+// const button = document.createElement('button');
+// button.innerText = 'remove item';
+// contentWrapper.appendChild(button);
+// input.appendChild(contentWrapper);
+// console.log(input);
+
+// const formBody = document.querySelector('input');
